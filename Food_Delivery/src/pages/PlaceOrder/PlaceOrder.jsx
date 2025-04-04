94b7f56598b8ff67c826f7f2a0f81c2bba5bd35a
Vivek Vishwakarma <166617357+vivekv88@users.noreply.github.com>
2025-04-04 10:47:36 +0000

    order

--- Food_Delivery/src/pages/PlaceOrder/PlaceOrder.jsx
+++ Food_Delivery/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -38,12 +38,12 @@ const PlaceOrder = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>${2}</p>
+              <p>${getTotalCartAmount()===0?0:2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount()+2}</b>
+              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
             </div>
             <button onClick={()=>navigate('/order')}>PROCEED TO PAYMENT</button>
           </div>
