3824e3838c327c117e11703359e095617b781b89
Vivek Vishwakarma <166617357+vivekv88@users.noreply.github.com>
2025-04-09 04:39:41 +0000

    login page done

--- Food_Delivery/backend/controllers/userController.js
+++ Food_Delivery/backend/controllers/userController.js
@@ -6,7 +6,28 @@ import validator from 'validator'
 // login user function
 
 const loginUser = async (req, res) => {
+    const {email,password} = req.body;
 
+    try {
+        const user = await userModel.findOne({email})
+
+        if(!user){
+            return res.json({success:false,message:"User doesn't exists Please register"})
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password);
+
+        if(!isMatch){
+            return res.json({success:false,message:"Invalid email or Password"})
+        }
+
+        const token = createToken(user._id)
+        res.json({success:true,token})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
 }
 
 const createToken = (id) => {
