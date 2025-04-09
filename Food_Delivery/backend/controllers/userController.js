3ce67c06e337b51852b3632c7dd7fabd087f88d4
Vivek Vishwakarma <166617357+vivekv88@users.noreply.github.com>
2025-04-09 03:44:57 +0000

    user part

---
+++ Food_Delivery/backend/controllers/userController.js
@@ -0,0 +1,18 @@
+import userModel from "../models/userModel.js";
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import validator from 'validator'
+
+// login user function
+
+const loginUser = async (req,res) => {
+
+}
+
+// register user function
+
+const registerUser = async (req,res) => {
+
+}
+
+export {loginUser,registerUser}
