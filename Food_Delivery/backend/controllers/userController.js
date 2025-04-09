f88d40c03f1700cb609d2e1d881401831646e050
Vivek Vishwakarma <166617357+vivekv88@users.noreply.github.com>
2025-04-09 04:26:20 +0000

    complete user registration part

--- Food_Delivery/backend/controllers/userController.js
+++ Food_Delivery/backend/controllers/userController.js
@@ -5,14 +5,55 @@ import validator from 'validator'
 
 // login user function
 
-const loginUser = async (req,res) => {
+const loginUser = async (req, res) => {
 
+}
+
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET);
 }
 
 // register user function
 
-const registerUser = async (req,res) => {
+const registerUser = async (req, res) => {
 
+    const { name, password, email } = req.body;
+
+    try {
+        // check for existing user
+        const userExists = await userModel.findOne({ email })
+        if (userExists) {
+            return res.json({ success: false, message: "User already exists" })
+        }
+
+        // validating email and password
+        if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: "Please enter a valid email" })
+        }
+
+        // strong password
+        if (password.length < 8) {
+            return res.json({ success: false, message: "Please enter a strong password" })
+        }
+
+        //hashing user password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // creating user account
+        const newUser = new userModel({
+            name: name,
+            email: email,
+            password: hashedPassword
+        })
+
+        const user = await newUser.save();
+        const token = createToken(user._id)
+        res.json({success:true,token})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
 }
 
-export {loginUser,registerUser}
+export { loginUser, registerUser }
