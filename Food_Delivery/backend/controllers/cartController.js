1a1de4b60b72eaf1f829c9eb9d0ee1464de13851
Vivek Vishwakarma <166617357+vivekv88@users.noreply.github.com>
2025-04-09 07:16:55 +0000

    creating cart logic

---
+++ Food_Delivery/backend/controllers/cartController.js
@@ -0,0 +1,16 @@
+import userModel from '../models/userModel.js'
+
+// add items to user cart
+const addToCart = async (req,res) => {
+
+}
+
+// remove items from the cart
+const removeFromCart = async (req,res) => {
+
+}
+
+// fetch user cart data
+const getCart = async (req,res) => {
+    
+}
