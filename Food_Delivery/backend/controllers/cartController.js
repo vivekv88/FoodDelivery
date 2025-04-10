962e0b45e7d885fdddee809a71e114860579ba44
Vivek Vishwakarma <166617357+vivekv88@users.noreply.github.com>
2025-04-10 03:45:16 +0000

    add to cart method done

--- Food_Delivery/backend/controllers/cartController.js
+++ Food_Delivery/backend/controllers/cartController.js
@@ -1,18 +1,33 @@
 import userModel from '../models/userModel.js'
 
 // add items to user cart
-const addToCart = async (req,res) => {
+const addToCart = async (req, res) => {
+    const userData = await userModel.findOne({ _id: req.body.userId });
+    const cartData = await userData.cartData
 
+    try {
+        if (!cartData[req.body.itemId]) {
+            cartData[req.body.itemId] = 1
+        }
+        else {
+            cartData[req.body.itemId] += 1
+        }
+        await userModel.findByIdAndUpdate(req.body.userId, { cartData })
+        res.json({ success: true, message: "Added to Cart" })
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
 }
 
 // remove items from the cart
-const removeFromCart = async (req,res) => {
-
+const removeFromCart = async (req, res) => {
+    
 }
 
 // fetch user cart data
-const getCart = async (req,res) => {
+const getCart = async (req, res) => {
 
 }
 
-export {addToCart,removeFromCart,getCart}
+export { addToCart, removeFromCart, getCart }
