74de71841faa0115b83182ffb4e5df82bd2253ca
Vivek Vishwakarma <166617357+vivekv88@users.noreply.github.com>
2025-04-11 10:13:44 +0000

    admin order page

--- Food_Delivery/backend/routes/orderRoute.js
+++ Food_Delivery/backend/routes/orderRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import authMiddleware from '../middlewares/auth.js'
-import { placeOrder,userOrders,verifyOrder } from "../controllers/orderController.js";
+import { listOrders, placeOrder,userOrders,verifyOrder } from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/place",authMiddleware,placeOrder)
 orderRouter.post("/verify",verifyOrder)
 orderRouter.post("/userOrders",authMiddleware,userOrders)
+orderRouter.get("/list",listOrders)
 
 export default orderRouter
