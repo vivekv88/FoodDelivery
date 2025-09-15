438d47db28fff81fe0ef0b7fb4ea773214e50375
Vivek Vishwakarma <166617357+vivekv88@users.noreply.github.com>
2025-09-15 14:47:09 +0530

    Update orderController.js

--- Food_Delivery/backend/controllers/orderController.js
+++ Food_Delivery/backend/controllers/orderController.js
@@ -25,7 +25,7 @@ const placeOrder = async (req, res) => {
                 product_data: {
                     name: item.name
                 },
-                unit_amount: item.price * 100
+                unit_amount: item.price * 100 * 80
             },
             quantity: item.quantity
         }))
@@ -36,7 +36,7 @@ const placeOrder = async (req, res) => {
                 product_data: {
                     name: "Delivery Charges"
                 },
-                unit_amount: 2*100,
+                unit_amount: 2 * 100 * 80,
             },
             quantity:1
         })
