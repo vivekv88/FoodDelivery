3249646e6dd4936b5a9f23b221e1ebf733c11309
Vivek Vishwakarma <166617357+vivekv88@users.noreply.github.com>
2025-09-15 14:25:28 +0530

    Update Cart.jsx

--- Food_Delivery/frontend/src/pages/Cart/Cart.jsx
+++ Food_Delivery/frontend/src/pages/Cart/Cart.jsx
@@ -56,7 +56,7 @@ const Cart = () => {
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+              <b>₹{getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
             </div>
             <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
           </div>
