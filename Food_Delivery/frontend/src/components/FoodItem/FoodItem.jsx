ef679617da216f64658bea694ed1f5a4edf051c3
Vivek Vishwakarma <166617357+vivekv88@users.noreply.github.com>
2025-09-15 13:54:32 +0530

    Update FoodItem.jsx

--- Food_Delivery/frontend/src/components/FoodItem/FoodItem.jsx
+++ Food_Delivery/frontend/src/components/FoodItem/FoodItem.jsx
@@ -26,7 +26,7 @@ const FoodItem = ({id,name,description,price,image}) => {
                 <img src={assets.rating_starts} alt="" />
             </div>
             <p className='food-item-desc'>{description}</p>
-            <p className='food-item-price'>${price}</p>
+            <p className='food-item-price'>₹{price}</p>
         </div>
     </div>
   )
